0dbd2503eeb96d28fd14c40d124a3f69767d718c
Anthony Eryan <72895267+a-eryan@users.noreply.github.com>
2026-05-31 13:32:26 -0400

    Add: two-integer-sum-ii - submission-36

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-36.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    /*
+    solution 1: O(n) time complexity, O(n) space complexity
+    solution 2: O(n^2) time complexity, O(1) space complexity
+    solution 3: O(n) time complexity, O(1) space complexity
+    */
+    twoSum(numbers, target) {
+        let leftptr = 0;
+        let rightptr = numbers.length - 1;
+
+        while (numbers[leftptr] + numbers[rightptr] != target) {
+            if (numbers[leftptr] + numbers[rightptr] > target) {
+                console.log('greater than target');
+                rightptr -= 1;
+                console.log(rightptr)
+            }
+            if (numbers[leftptr] + numbers[rightptr] < target) {
+                console.log('less than target');
+                leftptr += 1;
+                console.log(leftptr)
+            }            
+        }
+        return [leftptr + 1, rightptr + 1];
+    }
+}
